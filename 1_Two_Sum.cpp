dbf901417896b13f26a04233ae4ba7282edec3fc
方雨寒 <fangyuhan@macbook-pro.lan>
2022-01-10 14:28:07 -0800

    initual commit, solution for leetcode 1:Two Sum

---
+++ 1_Two_Sum.cpp
@@ -0,0 +1,37 @@
+#include "general_include.hpp"
+using namespace std;
+void printVector(vector<int> nums)
+{
+    for(auto i:nums)
+    {
+        cout<<i<<endl;
+    }
+}
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        int n= nums.size();
+        unordered_map<int,int> res;
+        res[nums[0]]=0;
+        vector<int> answer(2);
+        for(int i=1;i<n;i++)
+        {
+            int temp = target - nums[i];
+            if(res.count(temp)==1)
+            {
+                answer[0]=res[temp];
+                answer[1] =i ;
+            }
+            res[nums[i]]=i;
+        }
+        printVector(answer);
+        return answer;
+    }
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(3);
+    nums.push_back(2);
+    nums.push_back(4);
+    twoSum(nums,6);
+}
